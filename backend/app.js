9ce0233e6ebbeb64d9de00b928a9e5c31e7604fc
Methsavi <135610100+Methsavi@users.noreply.github.com>
2026-01-02 18:12:48 +0530

    Implement Showtime feature with Admin/Customer Backend

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const movieRoutes = require("./routes/movieRoutes");
 const authRoutes = require("./routes/auth");
 const passwordRoutes = require("./routes/password");
+const showtimeRoutes = require('./routes/showtimeRoutes');
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use("/api/movies", movieRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/password", passwordRoutes);
+app.use('/api/showtimes', showtimeRoutes);
 
 module.exports = app;
