15a96b935d04beaa0295dcdeaae9d359d8f39dc6
Gimhani <silvagsd@students.nsbm.ac.lk>
2026-01-22 02:14:29 +0530

    FEAT: Update CORS configuration to use environment variable for frontend URL

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json());
